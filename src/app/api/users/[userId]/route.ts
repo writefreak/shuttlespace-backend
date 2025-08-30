678bfa5886fe171441c79e1c12a1a96bb4de6036
Writefreak <iscribbledit@gmail.com>
2025-08-30 17:28:44 +0100

    check

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -3,13 +3,12 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Remove the explicit type annotation for the second argument
 export async function DELETE(
   req: Request,
-  { params }: { params: { userId: string } }
+  { params }: { params: { userId: string } } // This is the corrected line
 ) {
   try {
-    const { userId } = params; // Now you can directly access `userId` from `params`
+    const { userId } = params;
 
     const deletedUser = await prisma.user.delete({
       where: { id: userId },
